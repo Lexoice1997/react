1cf017e012400063244567fe3bab709053c5d798
Lexoice1997 <Lexoice1997@gmail.com>
2021-03-04 21:08:45 +0500

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <div className="img">
+                <img src="https://uploads-ssl.webflow.com/5e96913c9bac7c0b5cb3391c/5f44a7398c0cdf460857e744_img-image.jpg" alt="" />
+            </div>
+
+            <div className="posts">
+                <div className="ava">
+                    ava + description
+                </div>
+
+                <div className="my_posts">
+                    My posts
+                </div>
+
+                <div className="new_post">
+                    New post
+                </div>
+
+                <div className="post_1">
+                    post1
+                </div>
+
+                <div className="post_2">
+                    post2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
